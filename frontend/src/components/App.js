b7e9017215afb714037b89fc34cd0c4b1fe87d61
vedanti_06 <vedantidantwala@gmail.com>
2021-09-11 01:04:58 +0530

    sign in sign up

---
+++ frontend/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SignIn from './SignIn'
+import SignUp from './SignUp';
+function App() {
+    return (
+        <div>
+            
+            <SignUp />
+        </div>
+    );
+}
+
+export default App;
