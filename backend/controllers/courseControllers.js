7ad14f808efed1cc31a8ce567ed1a89fcd45062e
Raj <rajsanghavi9@gmail.com>
2021-09-10 10:56:15 +0530

    Routes for getting courses by types

--- backend/controllers/courseControllers.js
+++ backend/controllers/courseControllers.js
@@ -5,6 +5,11 @@ const Course = require("../models/courseModel");
 LIST OF CONTROLLERS
 1. Create a new course - only instructors can
 2. Get all courses - for every user
+3. Get all frontend courses - for every user
+4. Get all backend courses - for every user
+5. Get all database courses - for every user
+6. Get all fullstack courses - for every user
+7. Get all designing courses - for every user
 */
 
 // Create a new course
@@ -68,7 +73,72 @@ const getAllCourses = asyncHandler(async (req, res) => {
   }
 });
 
+// Get details of all Frontend courses
+const getAllFrontendCourses = asyncHandler(async (req, res) => {
+  const courses = await Course.find({ type: "frontend" });
+  if (courses.length > 0) {
+    res.status(200).json(courses);
+  } else {
+    res.status(404).json({
+      message: "No Frontend Course found",
+    });
+  }
+});
+
+// Get details of all Backend courses
+const getAllBackendCourses = asyncHandler(async (req, res) => {
+  const courses = await Course.find({ type: "backend" });
+  if (courses.length > 0) {
+    res.status(200).json(courses);
+  } else {
+    res.status(404).json({
+      message: "No Backend Course found",
+    });
+  }
+});
+
+// Get details of all Database courses
+const getAllDatabaseCourses = asyncHandler(async (req, res) => {
+  const courses = await Course.find({ type: "database" });
+  if (courses.length > 0) {
+    res.status(200).json(courses);
+  } else {
+    res.status(404).json({
+      message: "No Database Course found",
+    });
+  }
+});
+
+// Get details of all Fullstack courses
+const getAllFullstackCourses = asyncHandler(async (req, res) => {
+  const courses = await Course.find({ type: "fullstack" });
+  if (courses.length > 0) {
+    res.status(200).json(courses);
+  } else {
+    res.status(404).json({
+      message: "No Fullstack Course found",
+    });
+  }
+});
+
+// Get details of all Designing courses
+const getAllDesigningCourses = asyncHandler(async (req, res) => {
+  const courses = await Course.find({ type: "designing" });
+  if (courses.length > 0) {
+    res.status(200).json(courses);
+  } else {
+    res.status(404).json({
+      message: "No Designing Course found",
+    });
+  }
+});
+
 module.exports = {
   createCourse,
   getAllCourses,
+  getAllFrontendCourses,
+  getAllBackendCourses,
+  getAllDatabaseCourses,
+  getAllFullstackCourses,
+  getAllDesigningCourses,
 };
--- backend/routes/courseRoutes.js
+++ backend/routes/courseRoutes.js
@@ -3,6 +3,11 @@ const express = require("express");
 const {
   createCourse,
   getAllCourses,
+  getAllFrontendCourses,
+  getAllBackendCourses,
+  getAllDatabaseCourses,
+  getAllFullstackCourses,
+  getAllDesigningCourses,
 } = require("../controllers/courseControllers");
 
 const { instructorProtect } = require("../middlewares/protectedRoutes");
@@ -14,5 +19,20 @@ router.route("/createCourse").post(instructorProtect, createCourse);
 
 // Get all courses
 router.route("/allCourses").get(getAllCourses);
+
+// Get all Frontend courses
+router.route("/allFrontendCourses").get(getAllFrontendCourses);
+
+// Get all Backend courses
+router.route("/allBackendCourses").get(getAllBackendCourses);
+
+// Get all Database courses
+router.route("/allDatabaseCourses").get(getAllDatabaseCourses);
+
+// Get all Fullstack courses
+router.route("/allFullstackCourses").get(getAllFullstackCourses);
+
+// Get all Designing courses
+router.route("/allDesigningCourses").get(getAllDesigningCourses);
 
 module.exports = router;
